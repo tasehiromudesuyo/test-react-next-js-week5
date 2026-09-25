4eacc6a1546bcf3d3ed0c523104adeee41a2608f
tase <tase1240@gmail.com>
2026-09-26 08:37:24 +0900

    update page.js

--- app/Counter.js
+++ app/Counter.js
@@ -1,18 +1,27 @@
 "use client";
-import { useState } from "react"; // useState をインポート
 
+import { useState } from "react";
 
 export default function Counter() {
-    const [count,setCount] = useState(0);
 
-    const incrementCount = () => {
-      setCount(count + 1);  
-    };
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-          <p>現在のカウント: {count}</p>
-          <button onClick={incrementCount}>カウントを増やす</button> 
-        </div>
-    );
+  const incrementCount = () => {
+
+    setCount(count + 1);
+
+  };
+
+  return (
+
+    <div>
+
+      <p>現在のカウント: {count}</p>
+
+      <button onClick={incrementCount}>カウントを増やす</button>
+
+    </div>
+
+  );
+
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./Counter";
+import styles from "./page.module.css";
 import Counter from "./Counter";
 export default function Home() {
   return (
